e04be58e6ae1267d5aa333cf4cf16a97ac2407ec
MARCOS SOARES <marcos.soares@emiratesskills.ae>
2025-06-21 12:55:17 +0400

    Setup the nuget packages, dbContext, appsettings and the program CS.

---
+++ ApiFaceAi/ApiFaceAi/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace ApiFaceAi.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }   
+        public byte[] PhotoBlob { get; set; }
+        public string RekognitionFaceId { get; set; }   
+    }
+}
